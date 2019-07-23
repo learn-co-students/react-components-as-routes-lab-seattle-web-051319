a1de5e32b46477d1ae82228cf9386f4ce3db4dab
AustinBH <harlowaustin2@gmail.com>
2019-07-23 13:39:28 -0700

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+  const showMovies = movies => {
+    return movies.map(movie => {
+      return <li>{movie}</li>
+    })
+  }
+
+  const showDirectors = () => {
+    return directors.map(director => {
+      return <div>
+        <p>{director.name}</p>
+        Movies: <ul>{showMovies(director.movies)}</ul>
+      </div>
+    })
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {showDirectors()}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,29 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const showGenres = genres => {
+    return genres.map(genre => {
+      return <li>
+        {genre}
+      </li>
+    })
+  }
+
+  const showMovies = () => {
+    return movies.map(movie => {
+      return <div>
+        <p>Title: {movie.title}</p>
+        <p>Run Time: {movie.time} minutes</p>
+        Genres: <ul>{showGenres(movie.genres)}</ul>
+        </div>
+        })
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {showMovies()}
     </div>
   );
 };
